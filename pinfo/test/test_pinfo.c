cff638adaaf925f39632af3ce0dadd5e44f7d822
Alicia Rodriguez Taboada <alicia_rodriguez@ultimatesoftware.com>
2017-06-12 00:47:11 -0400

    comment out forks and move thread jobs

--- pinfo/test/test_pinfo.c
+++ pinfo/test/test_pinfo.c
@@ -42,29 +42,29 @@ int main() {
 
   /* create child processes and keep ‘em active */
 
-  child1_pid = fork();
-  child2_pid = fork();
-  child3_pid = fork();
+  // child1_pid = fork();
+  // child2_pid = fork();
+  // child3_pid = fork();
 
-  if (child1_pid != 0) {
-    child1_pid = wait(&child1_status);
-  }
+  // if (child1_pid != 0) {
+  //   child1_pid = wait(&child1_status);
+  // }
   // else {
   //   // printf("I am child1 with pid %d\n", child1_pid);
   //   exit(child1_status);
   // }
 
-  if (child2_pid != 0) {
-    child2_pid = wait(&child2_status);
-  }
+  // if (child2_pid != 0) {
+  //   child2_pid = wait(&child2_status);
+  // }
   // else {
   //   // printf("I am child2 with pid %d\n", child2_pid);
   //   exit(child2_status);
   // }
 
-  if (child3_pid != 0) {
-    child3_pid = wait(&child3_status);
-  }
+  // if (child3_pid != 0) {
+  //   child3_pid = wait(&child3_status);
+  // }
   // else {
   //   // printf("I am child3 with pid %d\n", child3_pid);
   //   exit(child3_status);
@@ -77,12 +77,12 @@ int main() {
   pthread_create(&thread2, NULL, print_message_function, (void*) message2);
   pthread_create(&thread3, NULL, print_message_function, (void*) message3);
 
+  /* If this is the parent process, call new system call */
+  status = syscall(301, p_info);
+
   pthread_join(thread1, NULL);
   pthread_join(thread2, NULL);
   pthread_join(thread3, NULL);
-
-  /* If this is the parent process, call new system call */
-  status = syscall(301, p_info);
 
   if (status < 0) {
     printf("Something went wrong. Status: %d\n", status);
@@ -93,7 +93,7 @@ int main() {
   printf("%d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %s\t\n",
   p_info->pid, p_info->state, p_info->nice, p_info->parent_pid, p_info->nr_children, p_info->nr_threads, p_info->youngest_child_pid, p_info->younger_sibling_pid, p_info->older_sibling_pid, p_info->start_time, p_info->user_time, p_info->sys_time, p_info->cutime, p_info->cstime, p_info->uid, p_info->comm);
 
-  sleep(10);
+  sleep(5);
 
   return 0;
 }
