a12a244c6eb4caef9cb4973c295ef001370c7228
Alicia Rodriguez Taboada <alicia_rodriguez@ultimatesoftware.com>
2017-06-12 23:25:37 -0400

    update test pinfo to properly create child processes

--- pinfo/test/test_pinfo.c
+++ pinfo/test/test_pinfo.c
@@ -26,68 +26,58 @@ void *thread_func(void *thread_id);
 
 int main() {
   struct pinfo *p_info;
-  int status;
-  long i;
-  pid_t pids[NUM_PROCESSES];
-  pthread_t threads[NUM_THREADS];
+  int status, pid;
+  long process, thread;
 
-  p_info = malloc(sizeof(*p_info));
-
-  // NOTE: The names of the pinfo struct fields have been abbriviated so they can be aligned nicely on the terminal.
-  printf("PID\t STATE\t NICE\t P_PID\t NR_C\t NR_T\t YCPID\t YSPID\t OSPID\t STIME\t UTIME\t SYTIM\t CUTIM\t CSTIM\t UID\t COMM\t\n");
+  // NOTE: The names of the pinfo struct fields have been abbreviated so they can be aligned nicely on the terminal.
+  printf("\nPID\t STATE\t NICE\t P_PID\t NR_C\t NR_T\t YCPID\t YSPID\t OSPID\t STIME\t UTIME\t SYTIM\t CUTIM\t CSTIM\t UID\t COMM\t\n");
 
   /* create child processes and keep ‘em active */
+  for (process = 0; process < NUM_PROCESSES; process++) {
+    pid = fork();
 
-  for (i = 0; i < NUM_PROCESSES; i++) {
-    pids[i] = fork();
-
-    if (pids[i] < 0) {
-      printf("Error");
+    if (pid < 0) {
       exit(1);
+    } else if (pid == 0) {  /* Child Process */
+      sleep(process);
+      exit(0);
+    } else {  /* Parent Process */
+
+      /* Create child threads and keep’em active */
+      pthread_t threads[NUM_THREADS];
+
+      for (thread = 0; thread < NUM_THREADS; thread++) {
+        pthread_create(&threads[thread], NULL, thread_func, (void *)thread);
+      }
+
+      /* Call new system call */
+      p_info = malloc(sizeof(*p_info));
+      status = syscall(301, p_info);
+
+      // Join Threads
+      for (thread = 0; thread < NUM_THREADS; thread++) {
+        pthread_join(threads[thread], NULL);
+      }
+
+      /* Output to standard output the p_info returned by new system call */
+      printf("%d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %s\t\n",
+      p_info->pid, p_info->state, p_info->nice, p_info->parent_pid, p_info->nr_children, p_info->nr_threads, p_info->youngest_child_pid, p_info->younger_sibling_pid, p_info->older_sibling_pid, p_info->start_time, p_info->user_time, p_info->sys_time, p_info->cutime, p_info->cstime, p_info->uid, p_info->comm);
     }
-    // else if (pids[i] == 0) {
-    //
-    //   // printf("Child...");
-    //   // exit(0);
-    // } else {
-    //   // Parent Process
-    //   // printf("waiting..");
-    //   wait(NULL);
-    // }
   }
 
-  /* Create child threads and keep’em active */
-
-  for (i = 0; i < NUM_THREADS; i++) {
-    pthread_create(&threads[i], NULL, thread_func, (void *)i);
+  for (process = 0; process < NUM_PROCESSES; process++) {
+    // Wait for child processes.
+    wait(NULL);
   }
-
-  /* If this is the parent process, call new system call */
-
-  status = syscall(301, p_info);
 
   if (status < 0) {
     printf("Something went wrong. Status: %d\n", status);
     return 0;
   }
 
-  for (i = 0; i < NUM_THREADS; i++) {
-    pthread_join(threads[i], NULL);
-  }
-
-  /* Output to standard output the p_info returned by new system call */
-
-  printf("%d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %s\t\n",
-  p_info->pid, p_info->state, p_info->nice, p_info->parent_pid, p_info->nr_children, p_info->nr_threads, p_info->youngest_child_pid, p_info->younger_sibling_pid, p_info->older_sibling_pid, p_info->start_time, p_info->user_time, p_info->sys_time, p_info->cutime, p_info->cstime, p_info->uid, p_info->comm);
-
-  sleep(5);
   return 0;
 }
 
 void *thread_func(void *thread_id) {
-  // long *id;
-  // id = (long *) thread_id;
-  // printf("Thread created with id %s", id);
-  // printf("%s \n", message);
-  // pthread_exit(NULL);
+  // Do nothing.
 }
